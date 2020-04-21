88a3d518a60f7dcc645a510dbf5ae1629ebf9d1f
Jeremy Ho <jujaga@gmail.com>
2020-04-21 12:38:16 -0700

    Implement rudimentary database dump functionality
    Signed-off-by: Jeremy Ho <jujaga@gmail.com>

--- app/src/services/dataService.js
+++ app/src/services/dataService.js
@@ -4,6 +4,24 @@ const constants = require('../components/constants');
 const db = require('../models');
 
 module.exports = {
+  dumpAll() {
+    return db.sequelize.transaction(async t => {
+      const businessObjs = await db.Business.findAll({}, { transaction: t });
+      const contactObjs = await db.Contact.findAll({}, { transaction: t });
+      const ipcPlanObjs = await db.IPCPlan.findAll({}, { transaction: t });
+
+      return businessObjs.map(b => {
+        const record = {
+          business: b,
+          contacts: contactObjs.filter(c => c.businessId === b.businessId),
+          ipcPlan: ipcPlanObjs.find(ipc => ipc.businessId === b.businessId)
+        };
+
+        return record;
+      });
+    });
+  },
+
   async save(business, contacts, ipcPlan) {
     const results = {};
     await db.sequelize.transaction(async t => {
