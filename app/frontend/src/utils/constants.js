5390e1754c94b5ac80b1c20044bacf0488d10082
Jeremy Ho <jujaga@gmail.com>
2020-04-21 14:53:39 -0700

    Implement frontend service layer for ipc calls
    Signed-off-by: Jeremy Ho <jujaga@gmail.com>

--- app/frontend/src/utils/constants.js
+++ app/frontend/src/utils/constants.js
@@ -1,11 +1,4 @@
 export const ApiRoutes = Object.freeze({
-  ACRONYMS: '/acronyms',
-  AUDIT: '/audit',
-  DOCS: '/docs',
   EMAIL: '/email',
-  HEALTH: '/checks/status',
-  KC_CLIENTS: '/keycloak/serviceClients',
-  KC_CONFIG: '/keycloak/configForm',
-  USERS: '/users',
-  WEBADE: '/webade'
+  IPC: '/ipc'
 });
