10865eddea89175eada64300f71edb61e31e030b
Csaky <Tim.Csaky@gov.bc.ca>
2020-05-05 13:42:31 -0700

    using baseWarningCard

--- app/frontend/src/components/form/OrgBookSearch.vue
+++ app/frontend/src/components/form/OrgBookSearch.vue
@@ -1,23 +1,31 @@
 <template>
-  <v-combobox
-    dense
-    outlined
-    flat
-    solo
-    v-model="model"
-    :rules="rules"
-    :items="items"
-    :loading="isLoading"
-    :search-input.sync="search"
-    v-on:change="change"
-    clearable
-    hide-no-data
-    hide-selected
-    label="OrgBook Lookup"
-    placeholder="Start typing to search the OrgBook database"
-    prepend-icon="mdi-database-search"
-    append-icon
-  ></v-combobox>
+  <div>
+    <v-combobox
+      dense
+      outlined
+      flat
+      solo
+      v-model="model"
+      :rules="rules"
+      :items="items"
+      :loading="isLoading"
+      :search-input.sync="search"
+      v-on:change="change"
+      v-on:blur="blur"
+      clearable
+      hide-no-data
+      hide-selected
+      label="OrgBook Lookup"
+      placeholder="Start typing to search the OrgBook database"
+      prepend-icon="mdi-database-search"
+      append-icon
+    ></v-combobox>
+    <!-- warning box -->
+    <BaseWarningCard v-if="showOrgBookWarning" class="mb-6">
+      <h3>You have entered a Business Name that is not recognised by BC Registries.</h3>
+      <p>This is no big deal.</p>
+    </BaseWarningCard>
+  </div>
 </template>
 
 <script>
@@ -37,6 +45,7 @@ export default {
       search: null,
       model: this.fieldModel,
       rules: this.fieldRules,
+      showOrgBookWarning: false,
     };
   },
   computed: {
@@ -65,7 +74,19 @@ export default {
         // For this use, want to emit just the text
         typeof value === 'object' && value !== null ? value.text : value
       );
+      // hide Org Book warning when user chooses from Org Book
+      this.showOrgBookWarning = false;
+
     },
+    blur(value) {
+      const inputValue = value.srcElement._value;
+      // get array of the suggestions from OrgBook
+      const arrayOfOrgBookValues = this.entries.map( result => {
+        return result.names[0].text;
+      });
+      // check if user input matched an Org Book suggestion
+      this.showOrgBookWarning = (arrayOfOrgBookValues.includes(inputValue)) ? false : true;
+    }
   },
   watch: {
     search(val) {
