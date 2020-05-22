f9935fa4e0f3f12cbfad726d8f8ecf356f7ce180
Csaky <Tim.Csaky@gov.bc.ca>
2020-05-22 15:51:42 -0700

    update directly from component instead of via the store

--- app/frontend/src/components/admin/inspection/LocationEdit.vue
+++ app/frontend/src/components/admin/inspection/LocationEdit.vue
@@ -1,7 +1,6 @@
 <template>
   <v-dialog v-model="dialog" max-width="800px">
     <template v-slot:activator="{ on }">
-      <!-- <v-btn color="primary" dark v-on="on">Open Dialog</v-btn> -->
       <v-btn v-if="true" v-on="on" color="primary" class="my-3" small>Edit Submission</v-btn>
     </template>
     <v-card>
@@ -21,7 +20,7 @@
           </v-container>
         </v-card-text>
         <v-card-actions class="pa-3 pb-10">
-          <v-btn color="primary" @click="update">Save</v-btn>
+          <v-btn color="primary" @click="submit">Save</v-btn>
           <v-btn text @click="cancelUpdate">Cancel</v-btn>
         </v-card-actions>
       </v-form>
@@ -31,9 +30,10 @@
 
 <script>
 import { mapGetters, mapActions } from 'vuex';
-import form from '@/store/modules/form.js';
+//import form from '@/store/modules/form.js';
 
 import Location from '@/components/form/Location.vue';
+import ipcService from '../../../services/ipcService';
 
 export default {
   name: 'LocationEdit',
@@ -48,34 +48,34 @@ export default {
   },
   data() {
     return {
-      showEditBtn: false,
       dialog: false,
       updateValid: false,
       reviewMode: false,
+      updateComplete: false,
+      updateError: '',
       startDateMenu: false,
-      endDateMenu: false,
+      endDateMenu: false
     };
   },
   computed: {
-    ...mapGetters('form', ['business', 'ipcPlan', 'location', 'submissionComplete']),
-    ...mapActions('form', ['updateForm']),
+    ...mapGetters('form', ['business', 'ipcPlan', 'location']),
 
-
-    // // Business data
+    // Business data
     businessName: {
       get() { return this.business.name; },
     },
 
   },
   methods: {
+    ...mapActions('form', ['getForm']),
 
-    async update() {
-
+    async submit() {
       if(this.$refs.form.validate()) {
-
         await this.updateForm();
-        if (this.submissionComplete) {
+        if (this.updateComplete) {
           this.dialog = false;
+          // reload form data to show in parent component;
+          this.getForm(this.ipcPlanId);
         }
       } else {
         const modal = document.querySelector('.v-dialog--active');
@@ -83,26 +83,42 @@ export default {
       }
     },
 
+    async updateForm() {
+      try {
+        const body = {
+          location: this.location
+        };
+        const response = await ipcService.updateIPCContent(body);
+        if (!response.data) {
+          throw new Error('No response data from API while submitting update');
+        }
+        this.updateComplete = true;
+      } catch (error) {
+        console.error(`Error submitting update: ${error}`); // eslint-disable-line no-console
+        this.updateError = 'An error occurred while attempting to update the form. Please try again.';
+      }
+    },
+
     async cancelUpdate(){
       // TODO reload location details in step2 component in case user changed details but didnt submit update
       this.dialog = false;
+      // reload form data to show in parent component;
+      this.getForm(this.ipcPlanId);
     },
 
   },
-  mounted() {
-  },
-  created() {
-    // dynamically register a ephemeral vuex module for this 'edit location' component
-    if(this.$store.hasModule('edit')) {
-      this.$store.unregisterModule('edit');
-    }
-    this.$store.registerModule('edit', form);
-  },
-  beforeDestroy() {
-    // unload this store
-    this.$store.unregisterModule('edit');
-  },
-
+  // my attempt to use a local copy of the store
+  // created() {
+  //   // dynamically register a ephemeral vuex module for this 'edit location' component
+  //   if(this.$store.hasModule('edit')) {
+  //     this.$store.unregisterModule('edit');
+  //   }
+  //   this.$store.registerModule('edit', form);
+  // },
+  // beforeDestroy() {
+  //   // unload this store
+  //   this.$store.unregisterModule('edit');
+  // },
 
 };
 </script>
