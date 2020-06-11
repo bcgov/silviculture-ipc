ba5abc89273a2914fcaa95e0f9962c5677341825
Csaky <Tim.Csaky@gov.bc.ca>
2020-06-11 14:50:40 -0700

    fixed backend constants format

--- app/src/components/constants.js
+++ app/src/components/constants.js
@@ -1,10 +1,7 @@
 module.exports = Object.freeze({
   SLEEPING_AREA_TYPE_SINGLE: 1,
   SLEEPING_AREA_TYPE_SHARED: 2,
-  SLEEPING_AREA_TYPES: [this.SLEEPING_AREA_TYPE_SINGLE, this.SLEEPING_AREA_TYPE_SHARED]
-});
-
-module.exports = Object.freeze({
+  SLEEPING_AREA_TYPES: [this.SLEEPING_AREA_TYPE_SINGLE, this.SLEEPING_AREA_TYPE_SHARED],
   OperationTypes: {
     BRUSHING: 'Brushing',
     CONE_PICKING: 'Cone Picking',
